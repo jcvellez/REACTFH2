e98c8f6dcd641b1cdc2ab8378addd3faa2dbf2d1
Juan Carlos Velez <jcvellez@live.com>
2023-04-29 11:04:30 -0500

    Me toca Parte 1 Carpeta 3 Clase 15 Operador ternario

--- 02-INTRO-JS/src/index.js
+++ 02-INTRO-JS/src/index.js
@@ -1,5 +1,36 @@
 //async 
 
-const getImagen = async()=> { return 'ffffffffffffffffffff'; } 
 
-getImagen().then( (uri)=> console.log(uri) );
+const getImagen = async () => {
+    const id=52;
+    const peticion = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    const sprites = await peticion.json();
+    return sprites;
+}
+getImagen().then(
+    ({ sprites }) => {
+        console.log(sprites.other.home.front_default)
+        const url = sprites.other.home.front_default;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append( img );
+    }
+
+);
+
+
+
+
+//
+// const peticion = fetch('https://pokeapi.co/api/v2/pokemon/8/');
+// peticion
+// .then( resp => resp.json() .then( ( {sprites} )=> {
+//     console.log(sprites.other.home.front_default);
+//     const { url } = sprites.other.home;
+//     const img = document.createElement('img');
+//     img.src = url;
+//     document.body.append( img );
+// })
+// )
+// .catch( console.warn() )
+// ;
