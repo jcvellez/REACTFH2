43402b991d09aeb1e0f3dc3a1da75877cdffa889
jcvellez <jcvellez@live.com>
2023-06-03 15:04:25 -0500

    Continua Carpeta 9 clase 9

--- 05-hook-app/src/components/02-useEffect/SimpleForm.js
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './effects.css';
 import Message from './Message';
+import useForm from './useForm';
+import FormWithCustomHook from './FormWithCustomHook';
 
 const SimpleForm = () => {
   const [formState, setformState] = useState(
@@ -14,57 +16,64 @@ const SimpleForm = () => {
   const { name, email } = formState;
 
   useEffect(() => {
-    //console.log('hey!!');
+    console.log('hey!!');
   }, []
   );
 
   useEffect(() => {
-    //console.log('formState Cambio');
-  }, [formState]);
+    console.log('formState Cambio');
+  }, [formState]); //Ok m detectas el formState
 
   useEffect(() => {
-    //console.log('email cambio');
-  }, [email]);
+    console.log('email cambio');
+  }, [email]); //Ok m detectas el email
 
-  const handleInputChange = ({ target }) => { //aqui desestructuro la e y saco el target solamente. Originalmente es asi => e.target.value
-    setformState(
-      {
-        ...formState,
-        [target.name]: target.value
-      }
-    );
-  }
+  // const handleInputChange = ({ target }) => { //aqui desestructuro la e y saco el target solamente. Originalmente es asi => e.target.value
+  //   setformState(
+  //     {
+  //       ...formState,
+  //       [target.name]: target.value
+  //     }
+  //   );
+  // }
 
+  // return (
+  //   <>
+  //     <h1>useEffect</h1>
+  //     <hr></hr>
+  //     <div className='form-group'>
+  //       <input
+  //         type='text'
+  //         name='name'
+  //         className='form-control'
+  //         placeholder='tu nombre'
+  //         autoComplete='off'
+  //         value={name}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+
+
+  //     <div className='form-group'>
+  //       <input
+  //         type='text'
+  //         name='email'
+  //         className='form-control'
+  //         placeholder='tu email'
+  //         autoComplete='off'
+  //         value={email}
+  //         onChange={handleInputChange}
+  //       />
+  //     </div>
+
+  //     {name === '123' && <Message />}
+  //   </>
+
+  //)
   return (
     <>
-      <h1>useEffect</h1>
-      <hr></hr>
-      <div className='form-group'>
-        <input
-          type='text'
-          name='name'
-          className='form-control'
-          placeholder='tu nombre'
-          autoComplete='off'
-          value={name}
-          onChange={handleInputChange}
-        />
-      </div>
-
-
-      <div className='form-group'>
-        <input
-          type='text'
-          name='email'
-          className='form-control'
-          placeholder='tu email'
-          autoComplete='off'
-          value={email}
-          onChange={handleInputChange}
-        />
-      </div>
-      
-      {name ==='123' && <Message />}
+      < FormWithCustomHook />
+      {/* {name === '123' && <Message />} */}
     </>
   )
 }
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,6 +4,7 @@ import HookApp from './HookApp';
 import { CounterApp } from './components/01-useState/CounterApp';
 import CounterWithcustomHook from './components/01-useState/CounterWithcustomHook';
 import SimpleForm from './components/02-useEffect/SimpleForm';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <FormWithCustomHook /> );
