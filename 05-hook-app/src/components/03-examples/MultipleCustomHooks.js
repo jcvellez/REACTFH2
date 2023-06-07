b9b7ef18ab42a8e7a4f1b62fee80407c7311dcf0
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2023-06-07 16:52:05 -0500

    Sigue la Carpeta 9 clase 10

--- 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import '../02-useEffect/effects.css'
+import useFetch from '../../hooks/useFetch'
 
 const MultipleCustomHooks = () => {
+
+const state = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/1`);
+console.log(state);
+
   return (
-    <div>Custom Hooks!!!!</div>
+    <div>
+      <h1>
+        Custom Hooks!!!!
+      </h1>
+    </div>
   )
 }
 
--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
 
-const useFetch = () => {
+const useFetch = (url) => {
+    const [state, setState] = useState(
+        {
+            data: null,
+            loading: null,
+            error: null
+        }
+    );
+    useEffect(() => {
+        fetch(url)
+            .then((resp => resp.json()))
+            .then(data => {
+                setState({
+                    loading: false,
+                    error: null,
+                    data: data
+                })
+            }
+
+            )
+        return state;
+    }, [])
+
+
 
 }
-
-//useFetch.propTypes = {}
 
 export default useFetch
